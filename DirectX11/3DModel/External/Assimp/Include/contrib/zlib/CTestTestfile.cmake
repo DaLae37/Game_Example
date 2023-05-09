c1a044522642459035127df9a552ca9fa760b643
DaLae37 <dalae37@gmail.com>
2023-05-09 19:57:58 +0900

    Remove Unnecessary Files (DirectX11 Assimp/Contrib)

--- DirectX11/3DModel/External/Assimp/Include/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Git/assimp/contrib/zlib
-# Build directory: D:/Git/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
